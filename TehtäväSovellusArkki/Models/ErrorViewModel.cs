e6b2a335830ea5efdc8cd91761868be47b4fe263
Karita Kamula <2306054@edu.karelia.fi>
2024-11-06 21:08:44 +0200

    Add project files.

---
+++ TehtäväSovellusArkki/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TehtäväSovellusArkki.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
